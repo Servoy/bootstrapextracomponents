690168737bb245d196ba1900dedbc7d15a626c53
Tuan Nguyen <tuanway@gmail.com>
2017-06-05 23:23:26 -0700

    Add Switch component

---
+++ bootstrapextracomponents/switch/switch.js
@@ -0,0 +1,101 @@
+angular.module('bootstrapextracomponentsSwitch', ['servoy', 'frapontillo.bootstrap-switch']).directive('bootstrapextracomponentsSwitch', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				name: "=",
+				model: "=svyModel",
+				handlers: "=svyHandlers",
+				api: "=svyApi",
+				svyServoyapi: "="
+			},
+			link: function($scope, $element, $attrs, $event) {
+
+				$scope.selection = false;
+				$scope.radioSelection = [];
+				$scope.isRadio = false;
+
+				$scope.$watch('model.dataProviderID', function() {
+						$scope.selection = getSelectionFromDataprovider();
+						setSelectionFromDataprovider();
+					})
+
+				$scope.$watch('model.valuelistID', function() {
+						if ($scope.model.valuelistID) {
+							$scope.isRadio = true;
+						} else {
+							$scope.isRadio = false;
+						}
+						if ($scope.svyServoyapi.isInDesigner() && !$scope.model.valuelistID) {
+							$scope.model.valuelistID = [{ realValue: 1, displayValue: "Item1" }, { realValue: 2, displayValue: "Item2" }, { realValue: 3, displayValue: "Item3" }];
+						}
+						if (!$scope.model.valuelistID) return; // not loaded yet
+						setSelectionFromDataprovider();
+					})
+
+				$scope.switchClicked = function($event, $index) {
+					//if we are using switch radio - only allow one item to be selected at a time.
+					if ($scope.isRadio) {
+						$scope.model.dataProviderID = $scope.model.valuelistID[$index].realValue;			
+						setSelectionFromDataprovider();
+					} else {
+						if ($scope.model.selection) {
+							$scope.model.dataProviderID = $scope.model.dataProviderID == $scope.model.selection ? null : $scope.model.selection;
+						} else if (angular.isString($scope.model.dataProviderID)) {
+							$scope.model.dataProviderID = $scope.model.dataProviderID == "1" ? "0" : "1";
+						} else {
+							$scope.model.dataProviderID = $scope.model.dataProviderID > 0 ? 0 : 1;
+						}
+					}
+					$scope.svyServoyapi.apply('dataProviderID');
+					$scope.handlers.onActionMethodID($event);
+
+				}
+
+				function setSelectionFromDataprovider() {
+					$scope.radioSelection = [];
+					if ($scope.model.dataProviderID === null || $scope.model.dataProviderID === undefined) return;
+					var arr = $scope.model.dataProviderID.split ? $scope.model.dataProviderID.split('\n') : [$scope.model.dataProviderID];
+					arr.forEach(function(element, index, array) {
+							for (var i = 0; i < $scope.model.valuelistID.length; i++) {
+								var item = $scope.model.valuelistID[i];
+								if (item.realValue && item.realValue == element)
+									$scope.radioSelection[i] = $scope.model.dataProviderID;
+							}
+					});
+				}
+
+				function getSelectionFromDataprovider() {
+					if ($scope.isRadio) {
+						var ret = "";
+						$scope.radioSelection.forEach(function(element, index, array) {
+							if (element == true) ret += $scope.model.valuelistID[index].realValue + '\n';
+						});
+						if (ret === "") ret = null
+						return ret;
+					} else {
+
+						if (!$scope.model.dataProviderID) return false;
+						if ($scope.model.selectedValue) {
+							return $scope.model.dataProviderID == $scope.model.selectedValue;
+						}
+						if (angular.isString($scope.model.dataProviderID)) {
+							return $scope.model.dataProviderID == "1";
+						} else {
+							return $scope.model.dataProviderID > 0;
+						}
+					}
+				}
+
+				/**
+				 * Request the focus to this switch.
+				 *
+				 * @example %%prefix%%%%elementName%%.requestFocus();
+				 */
+				$scope.api.requestFocus = function(mustExecuteOnFocusGainedMethod) {
+					$element.find('input')[0].focus();
+					
+				}
+			},
+			templateUrl: 'bootstrapextracomponents/switch/switch.html'
+		};
+	})
---
+++ bootstrapextracomponents/switch/switch.spec
@@ -0,0 +1,71 @@
+{
+	"name": "bootstrapextracomponents-switch",
+	"displayName": "Switch",
+	"version": 1,
+	"definition": "bootstrapextracomponents/switch/switch.js",
+	"libraries": [
+	{"name":"switch.css", "version":"1.0.0", "url":"bootstrapextracomponents/switch/switch.css", "mimetype":"text/css"},
+	{"name":"bootstrap-switch.css", "version":"3.3.4", "url":"bootstrapextracomponents/switch/bootstrap-switch.css", "mimetype":"text/css"},
+	{"name":"bootstrap-switch.js", "version":"3.3.4", "url":"bootstrapextracomponents/switch/bootstrap-switch.js", "mimetype":"text/javascript"},
+	{"name":"angular-bootstrap-switch.js", "version":"0.5.2", "url":"bootstrapextracomponents/switch/angular-bootstrap-switch.js", "mimetype":"text/javascript"}
+	],
+	"model":
+	{
+		 	"dataProviderID" : { "type":"dataprovider", "pushToServer": "allow", "tags": { "scope" :"design" }, "ondatachange": { "onchange":"onDataChangeMethodID", "callback":"onDataChangeCallback"}},
+	        "enabled" : { "type": "enabled", "blockingOn": false, "default": true, "for": ["dataProviderID","onActionMethodID","onDataChangeMethodID"] },
+	        "readOnly" : { "type": "protected", "blockingOn": true, "default": false, "for": ["dataProviderID","onDataChangeMethodID"], "tags": {"scope":"runtime"} },
+	        "styleClass" : { "type" :"styleclass", "tags": { "scope" :"design" }, "default":"switch"},
+	        "animate" : { "type" : "boolean" ,"default": true },
+	        "onText" : { "type" : "tagstring" ,"default": "On" },
+	        "offText" : { "type" : "tagstring" ,"default": "Off" },
+	        "onColor" : { "type" : "tagstring" ,"default": "primary" , "values":[{"Primary":"primary"},{"Info":"info"},{"Success":"success"},{"Warning":"warning"},{"Danger":"danger"}]},
+	        "offColor" : { "type" : "tagstring" ,"default": "primary" , "values":[{"Primary":"primary"},{"Info":"info"},{"Success":"success"},{"Warning":"warning"},{"Danger":"danger"}]},
+	        "radioOff" : {"type": "boolean", "default": true},	        
+	        "label" : { "type" : "tagstring" ,"default": "Switch" },
+	        "icon" : { "type" : "tagstring" ,"default": "" },
+	        "labelWidth" : { "type" : "tagstring" ,"default": "150" },
+	        "handleWidth" : { "type" : "tagstring" ,"default": "150" },
+	        "componentSize" : { "type" : "tagstring" ,"default": "Normal", "values":[{"Mini":"mini"},{"Small":"small"},{"Normal":"normal"},{"Large":"large"}] },
+			"tabSeq" : {"type" :"tabseq", "tags": { "scope" :"design" }},
+			"valuelistID" : { "type" : "valuelist", "tags": { "scope" :"design" }, "for": "dataProviderID"},
+			"selectedValue" : { "type" : "string" },			
+	        "visible" : "visible"
+	},
+	"handlers":
+	{
+	         "onActionMethodID" : {
+
+	        	"parameters":[
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								}
+							 ]
+	        },
+	        "onDataChangeMethodID" : {
+	          "returns": "boolean",
+
+	        	"parameters":[
+								{
+						          "name":"oldValue",
+								  "type":"${dataproviderType}"
+								},
+								{
+						          "name":"newValue",
+								  "type":"${dataproviderType}"
+								},
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								}
+							 ]
+	        }
+	},
+	"api":
+	{
+		"requestFocus": {
+				"delayUntilFormLoads": true,
+				"discardPreviouslyQueuedSimilarCalls": true
+	        }
+	}
+}
