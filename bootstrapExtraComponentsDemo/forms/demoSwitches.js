a2f938935a0b94bbb9898f8d4c961cd51e53f4e4
Tuan Nguyen <tuanway@gmail.com>
2017-08-02 15:48:17 -0500

    update switch demo

--- bootstrapExtraComponentsDemo/forms/demoSwitches.js
+++ bootstrapExtraComponentsDemo/forms/demoSwitches.js
@@ -4,3 +4,26 @@
  * @properties={typeid:35,uuid:"10A417B8-C195-4FC6-851E-7E084EFC9F1F",variableType:-4}
  */
 var switches_a = true;
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ * @param {String} color
+ * @private
+ *
+ * @properties={typeid:24,uuid:"A5CF4388-99AF-497A-A460-9CAF58F46486"}
+ */
+function onAction$updateColor(event, color) {
+	
+	for (var i=0; i<elements.allnames.length; i++)
+	{
+		var name = elements.allnames[i];
+		var elem = elements[name];
+		if (elem['onColor'] && elem['offColor']) {
+			elem['label'] = color
+			elem['onColor'] = color
+			elem['offColor'] = color
+		}
+	}	
+}
+
