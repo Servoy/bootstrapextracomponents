05547b4b8702035a6c64d7dec0df7db733564bb7
Tuan Nguyen <tuanway@gmail.com>
2017-08-02 16:46:44 -0500

    update demo

--- bootstrapExtraComponentsDemo/forms/demoSwitches.js
+++ bootstrapExtraComponentsDemo/forms/demoSwitches.js
@@ -1,6 +1,13 @@
 /**
  * @type {Boolean}
  *
+ * @properties={typeid:35,uuid:"54843FFA-BB0D-484B-A6DC-0387B3CD8474",variableType:-4}
+ */
+var enable = true;
+
+/**
+ * @type {Boolean}
+ *
  * @properties={typeid:35,uuid:"10A417B8-C195-4FC6-851E-7E084EFC9F1F",variableType:-4}
  */
 var switches_a = true;
@@ -14,15 +21,36 @@ var switches_a = true;
  * @properties={typeid:24,uuid:"A5CF4388-99AF-497A-A460-9CAF58F46486"}
  */
 function onAction$updateColor(event, color) {
-	
-	for (var i=0; i<elements.allnames.length; i++)
-	{
+
+	for (var i = 0; i < elements.allnames.length; i++) {
 		var name = elements.allnames[i];
 		var elem = elements[name];
-		if (elem['onColor'] && elem['offColor']) {					
+		if (elem['onColor'] && elem['offColor']) {
 			elem['onColor'] = color;
 			elem['offColor'] = color;
 		}
-	}	
+	}
 }
 
+/**
+ * @param oldValue
+ * @param newValue
+ * @param {JSEvent} event
+ *
+ * @return {boolean}
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"A80867E5-6DA4-439F-A91F-6C2EE61DD2E9"}
+ */
+function onDataChangeMethodID(oldValue, newValue, event) {
+	for (var i = 0; i < elements.allnames.length; i++) {
+		var name = elements.allnames[i];
+		var elem = elements[name];		
+		if (elem['onColor'] && elem['offColor'] && name!='switch_enable') {
+			elem.enabled = enable == 1 ? true : false;
+		}
+	}
+
+	return false;
+}
