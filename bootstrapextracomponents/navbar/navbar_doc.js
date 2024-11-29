0401110083e9e84819c6db2350f88405980ef343
lvostinar <lvostinar@servoy.com>
2024-11-29 16:33:46 +0200

    SVY-19717 Documentation | Navbar component

--- bootstrapextracomponents/navbar/navbar_doc.js
+++ bootstrapextracomponents/navbar/navbar_doc.js
@@ -1,6 +1,9 @@
 /**
  * Sets the menu items of the Navbar
  * 
+ * @example
+ * %%elementName%%.setMenuItems(menuItems)
+ * 
  * @param {Array<CustomType<bootstrapextracomponents-navbar.menuItem>>} menuItems
  */
 function setMenuItems(menuItems) {
@@ -8,6 +11,9 @@ function setMenuItems(menuItems) {
 
 /**
  * Adds the given menu item to the Navbar
+ * 
+ * @example
+ * %%elementName%%.addMenuItem(%%elementName%%.createMenuItem('my menu'))
  * 
  * @param {CustomType<bootstrapextracomponents-navbar.menuItem>} menuItem the menuItem to add
  * @param {Number} [index] optional index where the item will be inserted
@@ -17,6 +23,9 @@ function addMenuItem(menuItem, index) {
 
 /**
  * Creates a new menuItem
+ * 
+ * @example
+ * %%elementName%%.addMenuItem(%%elementName%%.createMenuItem('my menu'))
  * 
  * @param {String} text the item's text
  * @param {String} [itemId] optional ID to identify the item in scripting
@@ -29,6 +38,9 @@ function createMenuItem(text, itemId, position) {
 /**
  * Removes the menu item with the given item ID
  * 
+ * @example
+ * %%elementName%%.removeMenuItem('itemID')
+ * 
  * @param {String} menuItemId
  */
 function removeMenuItem(menuItemId) {
@@ -36,6 +48,9 @@ function removeMenuItem(menuItemId) {
 
 /**
  * Sets the menu item with the given item ID to selected
+ * 
+ * @example
+ * %%elementName%%.setMenuSelected('itemID')
  * 
  * @param {String} menuItemId
  */
@@ -45,6 +60,9 @@ function setMenuSelected(menuItemId) {
 /**
  * Enables or disables the menu with the given item ID
  * 
+ * @example
+ * %%elementName%%.setMenuItemEnabled('itemID',false)
+ * 
  * @param {String} menuItemId
  * @param {Boolean} enabled
  */
@@ -53,6 +71,9 @@ function setMenuItemEnabled(menuItemId, enabled) {
 
 /**
  * Enables or disables the submenu with the given item ID of the menu with the given item ID
+ * 
+ * @example
+ * %%elementName%%.setSubMenuItemEnabled('itemID','submenuID',false)
  * 
  * @param {String} menuItemId
  * @param {String} subMenuItemId
@@ -64,6 +85,9 @@ function setSubMenuItemEnabled(menuItemId, subMenuItemId, enabled) {
 /**
  * Returns the menu item with the given ID or null if not found
  * 
+ * @example
+ * var item = %%elementName%%.getMenuItem('itemID')
+ * 
  * @return {CustomType<bootstrapextracomponents-navbar.menuItem>}
  */
 function getMenuItem(itemId) {
@@ -72,6 +96,9 @@ function getMenuItem(itemId) {
 /**
  * Returns the currently selected menu item
  * 
+ * @example
+ * var item = %%elementName%%.getSelectedMenu()
+ * 
  * @return {CustomType<bootstrapextracomponents-navbar.menuItem>}
  */
 function getSelectedMenu() {
@@ -79,6 +106,11 @@ function getSelectedMenu() {
 
 /** 
  * Sets focus on the passed element on the navbar
+ * 
+ * @example
+ * %%elementName%%.requestFocus('itemId')
+ * 
+ * @param {String} menuItemId
  */
 function requestFocus(itemId) {
 }
@@ -86,12 +118,19 @@ function requestFocus(itemId) {
 /** 
  * Opens the submenu of the specified menu item.
  * 
+ * @example
+ * %%elementName%%.openSubMenu('itemId')
+ * 
+ * @param {String} menuItemId
  */
 function openSubMenu(itemId) {
 }
 
 /**
  * Retrieves the screen location of a specific navbar item. Returns the location as point (object with x and y properties).
+ * 
+ * @example
+ * var location = %%elementName%%.getLocation('itemID')
  * 
  * @param {string} itemId the node to retrieve location for.
  * @return {point} the location of the item.
@@ -101,6 +140,9 @@ function getLocation(itemId) {
 
 /**
  * Retrieves the size of a specific navbar item. Returns the size as dimension (object with width and height properties).
+ * 
+ * @example
+ * var size = %%elementName%%.getSize('itemID')
  * 
  * @param {string} itemId the node to retrieve size for.
  * @return {dimension} the size of the item.
